d2101d1d3f0d809483325dda7316866fc9c9555d
defapingan <x24251836@student.ncirl.ie>
2026-01-30 08:31:10 +0800

    1

---
+++ script.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+# Bash Script for DevOps Lab 2
+
+
+rm -rf fol_1 fol_2
+
+
+mkdir fol_1 fol_2
+
+
+touch fol_1/1_1.txt fol_1/1_2.txt fol_1/1_3.txt
+
+
+touch fol_2/2_1.txt fol_2/2_2.txt fol_2/2_3.txt
+
+
+chmod 600 fol_1/1_1.txt fol_1/1_3.txt
+chmod 600 fol_2/2_1.txt fol_2/2_3.txt
+
+
+chmod 777 fol_1/1_2.txt
+chmod 777 fol_2/2_2.txt
+
+echo "Job completed"
